19629075add9ebc0638029291f13e77729dd26a9
Markus Bauer <mb@karo-electronics.de>
2022-07-21 09:42:49 +0200

    rzg2l: add qsrz-g2l1 and txrz-g2l1 support

---
+++ plat/renesas/rz/board/qsrz-g2l1/rz_board.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2022, Markus Bauer <mb@karo-electronics.com>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+DDR_SOURCES +=  plat/renesas/rz/soc/${PLAT}/drivers/ddr/param_mc_qsrz-g2l1.c \
+		plat/renesas/rz/common/drivers/ddr/param_swizzle_qsrz-g2l1.c
+
+DDR_PLL4    := 1333
+$(eval $(call add_define,DDR_PLL4))
--- plat/renesas/rz/board/txrz-g2l0/rz_board.mk
+++ plat/renesas/rz/board/txrz-g2l0/rz_board.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
-DDR_SOURCES +=  plat/renesas/rz/soc/${PLAT}/drivers/ddr/param_mc_qsrz.c \
-		plat/renesas/rz/common/drivers/ddr/param_swizzle_qsrz.c
+DDR_SOURCES +=  plat/renesas/rz/soc/${PLAT}/drivers/ddr/param_mc_txrz-g2l0.c \
+		plat/renesas/rz/common/drivers/ddr/param_swizzle_txrz-g2l0.c
 
 DDR_PLL4    := 1333
 $(eval $(call add_define,DDR_PLL4))
